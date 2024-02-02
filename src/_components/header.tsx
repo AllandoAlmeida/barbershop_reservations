c2a2541a9856489ed757125d21b5f936fa50c512
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-02-02 09:32:30 -0300

    fix: fix bug auth

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const header = () => {
   return (
@@ -14,7 +16,7 @@ const header = () => {
           width={120}
         />
         <Button variant={"outline"} size={"icon"} className="h-8 w-8">
-          <MenuIcon size={18}/>
+          <MenuIcon size={18} />
         </Button>
       </CardContent>
     </Card>
